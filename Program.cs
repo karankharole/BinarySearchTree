c5d37036814fd8c5d584af3a39cdcaa20b30bf0f
karankharole <karankharole2000@gmail.com>
2023-01-19 19:42:48 +0530

    adding 56 and 30 and then 70

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySearchTreeNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Binary<int> binary = new Binary<int>(56);
+            binary.Add(30);
+            binary.Add(70);
+            binary.Display();
+            Console.ReadKey();
+        }
+    }
+}
